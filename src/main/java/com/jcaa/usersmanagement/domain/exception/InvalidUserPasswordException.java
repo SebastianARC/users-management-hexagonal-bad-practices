3772d21cae276b28c7d62683d39194d951a860ff
SebastianARC <sebastian.ramirezcamacho@unicolombo.edu.co>
2026-04-22 15:26:25 -0500

    refactor: extraer mensajes hardcodeados a constantes en InvalidUserPasswordException e InvalidUserRoleException (Regla 1 - item 10)

--- src/main/java/com/jcaa/usersmanagement/domain/exception/InvalidUserPasswordException.java
+++ src/main/java/com/jcaa/usersmanagement/domain/exception/InvalidUserPasswordException.java
@@ -2,16 +2,20 @@ package com.jcaa.usersmanagement.domain.exception;
 
 public final class InvalidUserPasswordException extends DomainException {
 
+  private static final String MSG_EMPTY =
+          "The user password must not be empty.";
+  private static final String MSG_TOO_SHORT =
+          "The user password must have at least %d characters.";
+
   private InvalidUserPasswordException(final String message) {
     super(message);
   }
 
   public static InvalidUserPasswordException becauseValueIsEmpty() {
-    return new InvalidUserPasswordException("The user password must not be empty.");
+    return new InvalidUserPasswordException(MSG_EMPTY);
   }
 
   public static InvalidUserPasswordException becauseLengthIsTooShort(final int minimumLength) {
-    return new InvalidUserPasswordException(
-        String.format("The user password must have at least %d characters.", minimumLength));
+    return new InvalidUserPasswordException(String.format(MSG_TOO_SHORT, minimumLength));
   }
 }
