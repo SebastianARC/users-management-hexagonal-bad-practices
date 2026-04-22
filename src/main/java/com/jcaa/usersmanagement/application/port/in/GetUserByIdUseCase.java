cf05edd64703d1cd6de75417ba00fec83083e352
SebastianARC <sebastian.ramirezcamacho@unicolombo.edu.co>
2026-04-22 14:28:29 -0500

    refactor: mover @Valid de implementacion a interfaz en GetUserById (Regla 1 - item 3)

--- src/main/java/com/jcaa/usersmanagement/application/port/in/GetUserByIdUseCase.java
+++ src/main/java/com/jcaa/usersmanagement/application/port/in/GetUserByIdUseCase.java
@@ -3,8 +3,7 @@ package com.jcaa.usersmanagement.application.port.in;
 import com.jcaa.usersmanagement.application.service.dto.query.GetUserByIdQuery;
 import com.jcaa.usersmanagement.domain.model.UserModel;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
 
 public interface GetUserByIdUseCase {
-  UserModel execute(@NotNull @Valid GetUserByIdQuery query);
+  UserModel execute(@Valid GetUserByIdQuery query);
 }
